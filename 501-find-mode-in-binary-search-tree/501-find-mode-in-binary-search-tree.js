cf4c3c5c985321301f8e22aadbaa18519dd7d002
Kenneth Tso <iamkennytso@gmail.com>
2022-01-25 00:22:15 -0500

    Time: 123 ms (52.29%), Space: 48.3 MB (20.26%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findMode = function(root) {
+  const obj = {}
+
+  const dig = node => {
+    if (!node) return
+    obj[node.val] ? obj[node.val]++ : obj[node.val] = 1
+    dig(node.left)
+    dig(node.right)
+  }
+  
+  dig(root)
+  
+  const largest = Math.max(...Object.values(obj))
+  return Object.keys(obj).filter(key => obj[key] === largest)
+  
+  console.log(obj)
+};
