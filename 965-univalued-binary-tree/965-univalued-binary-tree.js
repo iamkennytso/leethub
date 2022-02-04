15810d69e64ba04dc5da372eaaab452392e488dc
Kenneth Tso <iamkennytso@gmail.com>
2022-02-03 22:07:17 -0500

    Time: 92 ms (47.14%), Space: 41.9 MB (17.86%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.js
+++ 965-univalued-binary-tree/965-univalued-binary-tree.js
@@ -11,10 +11,7 @@
  * @return {boolean}
  */
 var isUnivalTree = function(root, val = root.val) {
-  // let helper = (node, val) => {
     if (!root) return true
     if (root.val !== val) return false
     return isUnivalTree(root.left, val) && isUnivalTree(root.right, val)
-  // }
-  // return isUnivalTree(root, root.val)
 };
