b1f6e33426ca7e3ad7d1af6e113ea96df0ad6645
Kenneth Tso <iamkennytso@gmail.com>
2022-06-24 15:11:41 -0400

    Time: 88 ms (93.56%), Space: 52 MB (10.12%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  const obj = {}
+  for (let num of nums) {
+    if (obj[num]) {
+      return true
+    }
+    obj[num] = true
+  }
+  return false
+};
