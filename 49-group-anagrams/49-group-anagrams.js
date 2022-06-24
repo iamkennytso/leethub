f69d64ab223013c3a075c67a9545d85b51aac4a3
Kenneth Tso <iamkennytso@gmail.com>
2022-06-24 15:49:42 -0400

    Time: 253 ms (20.02%), Space: 53.2 MB (66.32%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,8 +5,7 @@
 var groupAnagrams = function(strs) {
   const obj = {}
   for (let word of strs) {
-    const key = word.split('').sort()
-    console.log(key)
+    const key = word.split('').sort().join()
     obj[key] ? obj[key].push(word) : obj[key] = [word]
   }
   return Object.values(obj)
