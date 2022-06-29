03009a2f7f1e5ae96a0e0ecb62d8d7ff957ffa5f
Kenneth Tso <iamkennytso@gmail.com>
2022-06-29 13:21:36 -0400

    Time: 158 ms (45.22%), Space: 53.7 MB (76.08%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-  const arr = nums.map(_ => 1)
+  const arr = nums.map(() => 1)
   let prod = 1
-
   for (let i = 0; i < nums.length; i++) {
     arr[i] = arr[i] * prod
-    prod = nums[i] * prod
+    prod = prod * nums[i]
   }
-
   prod = 1
-
   for (let i = nums.length - 1; i >= 0; i--) {
     arr[i] = arr[i] * prod
-    prod = nums[i] * prod 
+    prod = prod * nums[i]
   }
-
+  
   return arr
+    
 };
