3d0adb1d745d27024bd5a828ce66fdbacbbba72e
Kenneth Tso <iamkennytso@gmail.com>
2022-07-05 01:01:55 -0400

    Time: 114 ms (19.79%), Space: 43.1 MB (48.04%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  let left = 0
+  let right = numbers.length - 1
+  while (left < right) {
+    const attempt = numbers[left] + numbers[right]
+    if (attempt === target) {
+      return [left + 1, right + 1]
+    } else if (attempt < target) {
+      left++
+    } else {
+      right--
+    }
+  }
+}; 
