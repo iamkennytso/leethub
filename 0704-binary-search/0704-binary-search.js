782cfbdee2641d330b493356fde26547fbc4802d
Kenneth Tso <iamkennytso@gmail.com>
2022-10-16 21:30:24 -0400

    Time: 101 ms (58.05%), Space: 45.4 MB (20.18%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  while(left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    console.log(mid, left, right)
+    const num = nums[mid]
+    if (num === target) {
+      return mid
+    } else if (num < target) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+  return -1
+};
