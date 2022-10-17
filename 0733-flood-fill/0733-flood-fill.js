e0e1c2b36e8a1d284e17889ddba9833a4d6b4220
Kenneth Tso <iamkennytso@gmail.com>
2022-10-17 18:52:08 -0400

    Time: 77 ms (93.18%), Space: 43.9 MB (83.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -2,19 +2,19 @@
  * @param {number[][]} image
  * @param {number} sr
  * @param {number} sc
- * @param {number} newColor
+ * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, newColor, targetColor = image[sr][sc], visited = {}) {
+var floodFill = function(image, sr, sc, color, curColor = image[sr][sc], visited = {}) {
   const key = sr + ',' + sc
-  if(!image[sr] || image[sr][sc] === undefined || image[sr][sc] !== targetColor || visited[key]) {
+  if (!image[sr] || image[sr][sc] !== curColor || visited[key]) {
     return image
   }
-  image[sr][sc] = newColor
   visited[key] = true
-  floodFill(image, sr+1, sc, newColor, targetColor,visited)
-  floodFill(image, sr-1, sc, newColor, targetColor,visited)
-  floodFill(image, sr, sc+1, newColor, targetColor,visited)
-  floodFill(image, sr, sc-1, newColor, targetColor,visited)
+  image[sr][sc] = color
+  floodFill(image, sr+1, sc, color, curColor, visited)
+  floodFill(image, sr-1, sc, color, curColor, visited)
+  floodFill(image, sr, sc+1, color, curColor, visited)
+  floodFill(image, sr, sc-1, color, curColor, visited)
   return image
 };
