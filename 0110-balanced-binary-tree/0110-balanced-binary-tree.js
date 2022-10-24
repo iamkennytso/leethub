530aa99d6619025d9371c92fd02f4978b56b1d87
Kenneth Tso <iamkennytso@gmail.com>
2022-10-24 13:54:46 -0400

    Time: 120 ms (58.22%), Space: 47.5 MB (39.01%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -10,29 +10,29 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-// var isBalanced = function(root) {
-//     const helper = node => {
-//       if (!node) return 0
-//       const left = helper(node.left)
-//       const right = helper(node.right)
-//       if (left === Infinity || right === Infinity || Math.abs(left - right) > 1) {
-//         return Infinity
-//       }
-//       return Math.max(left, right)
-//     }
-    
-//     return helper(root) === Infinity ? false : true
-// };
-
 var isBalanced = function(root) {
-  const helper = node => {
-    if (!node) return 0
-    const left = 1 + helper(node.left)
-    const right = 1 + helper(node.right)
-    if (Math.abs(left - right) > 1 || left === Infinity || right === Infinity) {
-      return Infinity
+    const helper = node => {
+      if (!node) return 0
+      const left = 1 + helper(node.left)
+      const right = 1 + helper(node.right)
+      if (left === Infinity || right === Infinity || Math.abs(left - right) > 1) {
+        return Infinity
+      }
+      return Math.max(left, right)
     }
-    return Math.max(left, right)
-  }
-  return helper(root) === Infinity ? false : true
+    
+    return helper(root) === Infinity ? false : true
 };
+
+// var isBalanced = function(root) {
+//   const helper = node => {
+//     if (!node) return 0
+//     const left = 1 + helper(node.left)
+//     const right = 1 + helper(node.right)
+//     if (Math.abs(left - right) > 1 || left === Infinity || right === Infinity) {
+//       return Infinity
+//     }
+//     return Math.max(left, right)
+//   }
+//   return helper(root) === Infinity ? false : true
+// };
