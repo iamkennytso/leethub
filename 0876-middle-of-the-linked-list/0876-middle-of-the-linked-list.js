265166c88efcc6ef3dcf4afbaf2fc4dba16ff237
Kenneth Tso <iamkennytso@gmail.com>
2022-11-01 19:35:40 -0400

    Time: 117 ms (7.39%), Space: 43.6 MB (6.37%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let length = 1
+  let cur = head
+  while (cur.next) {
+    length++
+    cur = cur.next
+  }
+
+  const mid = (Math.ceil(length / 2)) + (length % 2 === 0 ? 1 : 0)
+  console.log(mid)
+  cur = head
+  for (let i = 1; i < mid; i++) {
+    console.log(i)
+    cur = cur.next
+  }
+  return cur
+};
