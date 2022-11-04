f6f5cd87bb10278dff06c8f5202bfddc9c6072d3
Kenneth Tso <iamkennytso@gmail.com>
2022-11-04 00:29:14 -0400

    Time: 254 ms (6.64%), Space: 53.9 MB (13.78%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -4,8 +4,8 @@
  */
 var countBits = function(n) {
   const ans = []
-  for (let i = 0; i <= n; i++) {
-    ans.push(i.toString(2).split('').filter(c => c == 1).length)
+  for (let i = 0; i < n + 1; i++) {
+    ans.push(i.toString(2).split('').filter(num => num == 1).length)
   }
   return ans
 };
