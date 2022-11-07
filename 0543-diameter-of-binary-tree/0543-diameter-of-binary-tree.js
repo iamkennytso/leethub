4906257d1d8b8758fd7c039aeef7c6c231c0b102
Kenneth Tso <iamkennytso@gmail.com>
2022-11-06 19:48:05 -0500

    Time: 88 ms (83.81%), Space: 45.6 MB (68.62%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -1,19 +1,38 @@
-const diameterOfBinaryTree = root => {
-  if (!root) {
-    return 0;
-  }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
   let ans = 0
-  
-  const dig = root => {
-    if (!root) {
-      return 0;
-    }
-    const left = dig(root.left);
-    const right = dig(root.right);
+  const helper = node => {
+    if (!node) return 0
+    const left = helper(node.left)
+    const right = helper(node.right)
     ans = Math.max(ans, left + right)
     return 1 + Math.max(left, right)
   }
-
-  dig(root);
+  helper(root)
   return ans
 };
+
+
+//   const dig = root => {
+//     if (!root) {
+//       return 0;
+//     }
+//     const left = dig(root.left);
+//     const right = dig(root.right);
+//     ans = Math.max(ans, left + right)
+//     return 1 + Math.max(left, right)
+//   }
+
+//   dig(root);
+//   return ans
