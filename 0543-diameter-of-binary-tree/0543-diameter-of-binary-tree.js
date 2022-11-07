059a1efbae41c670d62ae58ec13b042fc77a2a30
Kenneth Tso <iamkennytso@gmail.com>
2022-11-07 11:08:46 -0500

    Time: 118 ms (49.62%), Space: 45.5 MB (68.62%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -15,24 +15,10 @@ var diameterOfBinaryTree = function(root) {
   const helper = node => {
     if (!node) return 0
     const left = helper(node.left)
-    const right = helper(node.right)
+    const right= helper(node.right)
     ans = Math.max(ans, left + right)
     return 1 + Math.max(left, right)
   }
   helper(root)
   return ans
 };
-
-
-//   const dig = root => {
-//     if (!root) {
-//       return 0;
-//     }
-//     const left = dig(root.left);
-//     const right = dig(root.right);
-//     ans = Math.max(ans, left + right)
-//     return 1 + Math.max(left, right)
-//   }
-
-//   dig(root);
-//   return ans
