c56bac450f36efe3edc1c4dbd430044b0672cfe1
Kenneth Tso <iamkennytso@gmail.com>
2022-11-08 20:53:19 -0500

    Time: 143 ms (33.09%), Space: 47.9 MB (19.11%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -5,11 +5,12 @@
 var singleNumber = function(nums) {
   const obj = {}
   for (let num of nums) {
-    if (num in obj) {
-      obj[num] = false
-    } else {
-      obj[num] = true
-    }
+    obj[num] ? obj[num] = false : obj[num] = true
+    // if (num in obj) {
+    //   obj[num] = false
+    // } else {
+    //   obj[num] = true
+    // }
   }
   return Object.keys(obj).find(key => obj[key] === true)
 };
