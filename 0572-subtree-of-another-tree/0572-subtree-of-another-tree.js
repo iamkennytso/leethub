868b48a56d24778a4ce1df1e6c73292b2b2ed8be
Kenneth Tso <iamkennytso@gmail.com>
2022-11-15 22:15:40 -0500

    Time: 140 ms (61.17%), Space: 49.6 MB (24.72%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -11,23 +11,15 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
+
+const helper = (p, q) => {
+  if (!p && !q) return true
+  if (!p || !q || p.val !== q.val) return false
+  return helper(p.left, q.left) && helper(p.right, q.right)
+}
+
 var isSubtree = function(root, subRoot) {
-  const helper = (p, q) => {
-    if (!p && !q) return true
-    if (!p || !q || p.val !== q.val) return false
-    return helper(p.left, q.left) && helper(p.right, q.right)
-  }
-
-  const queue = [root]
-
-  while (queue.length) {
-    const node = queue.shift()
-    if (node.val === subRoot.val) {
-     if (helper(node, subRoot)) return true
-    }
-    if (node.left) queue.push(node.left)
-    if (node.right) queue.push(node.right)
-  }
-
-  return false
+  if (helper(root, subRoot)) return true
+  if (!root) return false
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
 };
