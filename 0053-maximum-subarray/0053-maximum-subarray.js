d50c950b98cac019cdd9a064b708aed0f3d2db65
Kenneth Tso <iamkennytso@gmail.com>
2022-11-15 23:35:01 -0500

    Time: 151 ms (24.56%), Space: 50.1 MB (78.90%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,10 +3,17 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  const arr = new Array(nums.length)
-  arr[0] = nums[0]
+  // const arr = new Array(nums.length)
+  // arr[0] = nums[0]
+  // for (let i = 1; i < nums.length; i++) {
+  //   arr[i] = Math.max(nums[i], arr[i-1] + nums[i])
+  // }
+  // return Math.max(...arr)
+  let max = nums[0]
+  let prev = nums[0]
   for (let i = 1; i < nums.length; i++) {
-    arr[i] = Math.max(nums[i], arr[i-1] + nums[i])
+    max = Math.max(max, nums[i], prev + nums[i])
+    prev = Math.max(nums[i], prev + nums[i])
   }
-  return Math.max(...arr)
+  return max
 };
