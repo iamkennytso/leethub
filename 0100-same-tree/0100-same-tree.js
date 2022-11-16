8d85abf278e0bd10cc017edd41f4a2959db415d8
Kenneth Tso <iamkennytso@gmail.com>
2022-11-15 22:08:02 -0500

    Time: 81 ms (75.87%), Space: 42.1 MB (79.57%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,30 +12,7 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-  const pArr = []
-  const qArr = []
-  
-  const helper = (node, arr) => {
-    arr.push(node ? node.val : null)
-    if (!node) return
-    if (node.left !== null || node.right !== null) {
-      helper(node.left, arr)
-      helper(node.right, arr)
-    }
-  }
-  
-  helper(p, pArr)
-  helper(q, qArr)
-  
-  if (pArr.length !== qArr.length) {
-    return false
-  }
-  
-  for (let i = 0; i < pArr.length; i++) {
-    if (pArr[i] !== qArr[i]) {
-      return false
-    }
-  }
-
-  return true
+  if (!p && !q) return true
+  if (!p || !q || p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
