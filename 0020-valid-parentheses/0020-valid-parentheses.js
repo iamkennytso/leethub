6c55afd06d3268e37addccf39d706b9805360021
Kenneth Tso <iamkennytso@gmail.com>
2025-02-20 21:21:45 -0500

    Time: 2 ms (75.98%), Space: 56.9 MB (5.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,20 +3,19 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  const obj = {
-    '}' :'{',
-    ')': '(',
-    ']': '[' 
+  const map = {
+    "(": ")",
+    "{": "}",
+    "[": "]"
   }
   const stack = []
+  const openers = Object.keys(map)
   for (let char of s) {
-    if (char in obj) {
-      const comp = stack.pop()
-      if (comp !== obj[char]) {
-        return false
-      }
-    } else {
+    if (openers.includes(char)) {
       stack.push(char)
+    } else {
+      const opener = stack.pop()
+      if (map[opener] !== char) return false
     }
   }
   return stack.length === 0
