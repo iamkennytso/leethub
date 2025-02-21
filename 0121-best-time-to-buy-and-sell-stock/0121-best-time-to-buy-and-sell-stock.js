f02639e1de864a070f27858ee4f3269e37b96f8b
Kenneth Tso <iamkennytso@gmail.com>
2025-02-20 21:27:08 -0500

    Time: 7 ms (13.23%), Space: 65.6 MB (5.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let max = 0
-    let min = Infinity
-    for (let price of prices) {
-      min = Math.min(price, min)
-      max = Math.max(price - min, max)
-    }
-  return max
+  let lowest = Infinity
+  let maxProf = 0
+  for (let price of prices) {
+    lowest = Math.min(price, lowest)
+    maxProf = Math.max(price - lowest, maxProf)
+  }
+  return maxProf
 };
