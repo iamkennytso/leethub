9e5869e0f580e15a59f52df96e43464c130e2fba
Kenneth Tso <iamkennytso@gmail.com>
2025-02-20 21:22:18 -0500

    Time: 1 ms (90.2%), Space: 53.2 MB (6.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,13 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  const obj = {}
+var twoSum = function (nums, target) {
+  const complimentMap = {}
   for (let i = 0; i < nums.length; i++) {
-    const num = nums[i]
-    if (num in obj) {
-      return [obj[num], i]
+    const num = nums[i] // 2
+    if (num in complimentMap) {
+      return [i, complimentMap[num]]
+    } else {
+      const comp = target - num  // 7
+      complimentMap[comp] = i
     }
-    obj[target - num] = i
   }
 };
