16cd65c74b74811651ec53fc6ec5a2762ef31173
Kenneth Tso <iamkennytso@gmail.com>
2025-02-23 22:36:01 -0500

    Time: 0 ms (100%), Space: 53.6 MB (5.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,16 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-
-
-// 1 = 1
-// 2 = 2
-// 3 = 3
-// 4 = 5
-var climbStairs = function(n, memo = [0, 1, 2]) {
-  if (n in memo) {
-    return memo[n]
-  }
+var climbStairs = function(n, memo = {}) {
+  // 0 = 0
+  // 1 = 1
+  // 2 = 2
+  // 3 = 3
+  // 4 = 5
+  if (n <= 3) return n
+  if (n in memo) return memo[n]
   memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo)
   return memo[n]
 };
