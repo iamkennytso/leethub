75819b68fe25bb98bf3550bfaa649f8e3ba69782
Kenneth Tso <iamkennytso@gmail.com>
2025-02-24 17:51:46 -0500

    Time: 67 ms (72.29%), Space: 65.6 MB (5.21%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -15,7 +15,8 @@
 var lowestCommonAncestor = function(root, p, q) {
   if (!root) return null
   if (root === p || root === q) return root
-  if (lowestCommonAncestor(root.left, p, q) && lowestCommonAncestor(root.right, p, q)) {
-    return root
-  } else return lowestCommonAncestor(root.left, p, q) || lowestCommonAncestor(root.right, p, q)
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
+  if (left && right) return root
+  return left || right
 };
