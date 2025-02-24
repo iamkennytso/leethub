eee51ca44e4eb1d63c8aa31a2af01338e17cdb08
Kenneth Tso <iamkennytso@gmail.com>
2025-02-23 22:47:25 -0500

    Time: 6 ms (45.04%), Space: 57.1 MB (5.33%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-  const obj = {}
   let count = 0
-  for (let letter of s) {
-    obj[letter] = (obj[letter] || 0) + 1
-    if (obj[letter] % 2 === 0) {
-      count += 2
-    }
+  const countsMap = {}
+  for (let char of s) {
+    countsMap[char] = (countsMap[char] || 0) + 1
+    if (countsMap[char] % 2 === 0) count += 2
   }
-
   return count === s.length ? count : count + 1
 };
