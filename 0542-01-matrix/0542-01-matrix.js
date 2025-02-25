f0349cd15bc12eda161d86698b42e57b0c6d7704
Kenneth Tso <iamkennytso@gmail.com>
2025-02-25 03:52:54 -0500

    Time: 809 ms (5.05%), Space: 80 MB (5.18%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+  const newGrid = mat.map(_ => [])
+  const done = new Set()
+  const queue = []
+
+  const isInRange = (x, y) => {
+    const str = `${x},${y}`
+    const isGood =  mat[x] && mat[x][y] !== undefined && !done.has(str)
+    if (isGood) {
+      done.add(str)
+      return true
+    }
+    return false
+  }
+
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[0].length; j++) {
+      if (mat[i][j] === 0) {
+        done.add(`${i},${j}`)
+        queue.push([i, j, 0])
+      }
+    }
+  }
+
+  while (queue.length) {
+    const [x, y, val] = queue.shift()
+    const str = `${x},${y}`
+    console.log(x, y, val)
+    done.add(str)
+    newGrid[x][y] = val
+    if (isInRange(x + 1, y)) queue.push([x + 1, y, val + 1])
+    if (isInRange(x - 1, y)) queue.push([x - 1, y, val + 1])
+    if (isInRange(x, y + 1)) queue.push([x, y + 1, val + 1])
+    if (isInRange(x, y - 1)) queue.push([x, y - 1, val + 1])
+  }
+
+  return newGrid
+};
