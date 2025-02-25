8606acb4ab93d9a7dc626f73eea08ad22d611fb9
Kenneth Tso <iamkennytso@gmail.com>
2025-02-25 04:43:58 -0500

    Time: 47 ms (66.93%), Space: 71.8 MB (12.97%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -4,8 +4,5 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-  return points.map(point => {
-    const [x, y] = point
-    return [point, Math.sqrt((x*x) + (y * y))]
-  }).sort((a, b) => a[1] - b[1]).slice(0, k).map(tuple => tuple[0])
+    return points.sort((a, b) => Math.sqrt((a[0]*a[0]) + (a[1] * a[1])) - Math.sqrt((b[0]*b[0]) + (b[1] * b[1]))).slice(0, k)
 };
