8be11ba12f9a074229369bcb550f4aa09f8b5844
Kenneth Tso <iamkennytso@gmail.com>
2025-03-03 23:51:09 -0500

    Time: 0 ms (100%), Space: 57.9 MB (11.93%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,6 +11,8 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-  if (!root) return 0
-  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    if (!root) return 0
+    const left = maxDepth(root.left)
+    const right = maxDepth(root.right)
+    return 1 + Math.max(left, right)
 };
